79d38beb832e95a950bc763a6d48333781a36884
Carlos Andrés Beltran Tello <cabeltrantello@gmail.com>
2025-07-09 22:01:15 -0600

    Main router for API version 1. The prefix for management has changed.

--- Backend/App/Api/v1/api.py
+++ Backend/App/Api/v1/api.py
@@ -5,6 +5,6 @@ from Backend.App.Api.v1.Endpoints import management, simulator
 
 api_v1_router = APIRouter()
 
-api_v1_router.include_router(management.router, prefix="/_config", tags=["Management"])
+api_v1_router.include_router(management.router, prefix="/config", tags=["Management"])
 
 api_v1_router.include_router(simulator.router, tags=["Simulator"])
