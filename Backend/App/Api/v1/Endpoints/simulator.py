140bd2e67a497925a8cb836bac2c3e36a9a922be
Carlos Andrés Beltran Tello <cabeltrantello@gmail.com>
2025-07-09 21:59:38 -0600

    The "catch-all" endpoint, now aware of scenarios.

--- Backend/App/Api/v1/Endpoints/simulator.py
+++ Backend/App/Api/v1/Endpoints/simulator.py
@@ -1,12 +1,16 @@
 from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import JSONResponse
 from Backend.App.Db.in_memory_db import mock_db
+from Backend.App.Models.mock_request import DEFAULT_SCENARIO_NAME
 
 router = APIRouter()
 
 @router.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"])
+
 async def catch_all_simulator(request: Request, full_path: str):
     path_with_leading_slash = f"/{full_path}"
+
+    requested_scenario = request.query_params.get("scenario", DEFAULT_SCENARIO_NAME)
 
     found_mock = mock_db.find_mock(method=request.method, path=path_with_leading_slash)
 
@@ -14,13 +18,13 @@ async def catch_all_simulator(request: Request, full_path: str):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail={
-                "message": "No mock configuration found for the requested path and method.",
-                "requested_method" : request.method,
-                "requested_path" : path_with_leading_slash
+                "message": "No mock definition found for the requested path and method.",
+                "requested_method": request.method,
+                "requested_path": path_with_leading_slash
             }
         )
     
-    response_def = found_mock.response
+    response_def = found_mock.scenarios.get(requested_scenario, found_mock.scenarios[DEFAULT_SCENARIO_NAME])
 
     return JSONResponse(
         content=response_def.body,
